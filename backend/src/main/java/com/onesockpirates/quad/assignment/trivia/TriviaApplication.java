75f9bbb79ae4f051d1df3b2d59a11ef6144e3d3d
Jorge <jfernandez@topcon.com>
2025-10-24 11:42:02 +0200

    Amend

--- backend/src/main/java/com/onesockpirates/quad/assignment/trivia/TriviaApplication.java
+++ backend/src/main/java/com/onesockpirates/quad/assignment/trivia/TriviaApplication.java
@@ -1,5 +1,8 @@
 package com.onesockpirates.quad.assignment.trivia;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
