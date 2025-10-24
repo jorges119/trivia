ed47bf60dd78fa6e4e593f8c9288d0d79a35ff18
Jorge <jfernandez@topcon.com>
2025-10-24 11:36:37 +0200

    Fix swagger server url

--- backend/src/main/java/com/onesockpirates/quad/assignment/trivia/TriviaApplication.java
+++ backend/src/main/java/com/onesockpirates/quad/assignment/trivia/TriviaApplication.java
@@ -3,6 +3,11 @@ package com.onesockpirates.quad.assignment.trivia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition( 
+    servers = {
+       @Server(url = "/", description = "Default Server URL")
+    }
+) 
 @SpringBootApplication
 public class TriviaApplication {
 
